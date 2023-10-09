3fefe262d15e9055237a84b125c2b0d20a4a9c83
NielMc <>
2023-10-09 10:01:47 +0000

    Inherited blog templates from base

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"The title of this post is {self.title}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,10 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+
+def post_detail(request):
+    return render(request, "blog/post_detail.html")
