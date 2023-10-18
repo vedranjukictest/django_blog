dd90c44478fbc1e144cdb03569fd0dfb2f0395d9
NielMc <>
2023-10-18 16:28:40 +0000

    adds allauth capabilities

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("", include("blog.urls"), name='blog-urls'),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
