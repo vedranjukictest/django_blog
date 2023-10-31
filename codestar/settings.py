7de601df9b0b2f6273370c7512c4552e1e8ac423
NielMc <>
2023-10-31 15:40:17 +0000

    add mimetypes

--- codestar/settings.py
+++ codestar/settings.py
@@ -155,6 +155,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+WHITENOISE_MIMETYPES = {'.js': 'text/javascript'}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
